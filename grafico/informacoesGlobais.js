36152580983e5b5a24f338478ca57c89609fdddf
nanapatinha <117641525+nanapatinha@users.noreply.github.com>
2024-10-23 07:46:23 -0300

    Update and rename informacoesGlobais to informacoesGlobais.js

---
+++ grafico/informacoesGlobais.js
@@ -0,0 +1,10 @@
+const url = "";
+
+async function visualizarInformacoesGlobais
+  const res = await fetch(url)
+const dados = await res.json()
+console.log(dados);
+}
+
+visualizarInformacoesGlobais();
+
